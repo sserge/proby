58dd77b321af0eab25aa4dc82973f2aa7f7735b2
Sergey Staritsyn <47747738+sserge@users.noreply.github.com>
2020-05-06 15:48:46 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+(function(){
+
+  const canvas = document.getElementById('canvas')
+  const context = canvas.getContext('2d')
+  
+  canvas.width = 750
+  canvas.height = 750
+  
+  update()
+  function update() {
+    requestAnimationFrame(update)
+  }
+  
+})()
