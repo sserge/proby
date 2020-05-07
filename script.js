28c6461f977f884024a5c711706ea05ecbf4bb38
Sergey Staritsyn <47747738+sserge@users.noreply.github.com>
2020-05-07 13:53:21 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
 (async function(){
 
-  const image = await loadImage('image.jpg')
+//  const image = await loadImage('image.jpg')
   
   const canvas = document.getElementById('canvas')
-  const context = canvas.getContext('2d')
+//  const context = canvas.getContext('2d')
   
   canvas.width = 750
   canvas.height = 750
   
-  update()
-  function update () {
-    requestAnimationFrame(update)
-    clearCanvas()
-    context.drawImage(
-      image,
-      0, 0, image.width, image.height,
-      0, 0, canvas.width, canvas.height
-    )
-  }
-  function clearCanvas () {
-  }
+//  update()
+//  function update () {
+//    requestAnimationFrame(update)
+//    clearCanvas()
+//    context.drawImage(
+//      image,
+//      0, 0, image.width, image.height,
+//      0, 0, canvas.width, canvas.height
+//    )
+//  }
+//  function clearCanvas () {
+//  }
   
   
   
-function loadImage (src) {
-  return new Promise((resolve, reject) => {
-    try {
-      const image = new Image
-      image.onLoad = () => resolve(image)
-      image.src = src
-    } catch(err) {
-      return reject(err)
-    }
-  })
-}  
+//function loadImage (src) {
+//  return new Promise((resolve, reject) => {
+//    try {
+//      const image = new Image
+//      image.onLoad = () => resolve(image)
+//      image.src = src
+//    } catch(err) {
+//      return reject(err)
+//    }
+//  })
+//}  
   
 })()
