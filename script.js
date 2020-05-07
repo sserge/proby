81eb0476b73a6f06d5baeaa02b4683c5fb103124
Sergey Staritsyn <47747738+sserge@users.noreply.github.com>
2020-05-07 09:28:30 +0500

    Update script.js

--- script.js
+++ script.js
@@ -13,4 +13,18 @@
     requestAnimationFrame(update)
   }
   
+  
+  
+function loadImage (src) {
+  return new Promise((resolve, reject) => {
+    try {
+      const image = new Image
+      image.onLoad = () => resolve(image)
+      image.src = src
+    } catch(err) {
+      return reject(err)
+    }
+  })
+}  
+  
 })()
