1980b348b751c0bc3e6cd41c63cec1226d88b2e8
Sergey Staritsyn <47747738+sserge@users.noreply.github.com>
2020-05-07 08:29:09 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 (async function(){
 
-  const image = await loadImage('')
+  const image = await loadImage('image.jpg')
+  
   const canvas = document.getElementById('canvas')
   const context = canvas.getContext('2d')
   
