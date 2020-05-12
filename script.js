d3cfcbe1fd28b92b261dfa933c6947163d794df8
Sergey Staritsyn <47747738+sserge@users.noreply.github.com>
2020-05-12 16:47:44 +0500

    Update script.js

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ function loadImage (src) {
   return new Promise((resolve, reject) => {
     try {
       const image = new Image
-      image.onLoad = () => return resolve(image)
+      image.onLoad = () => resolve(image)
       image.src = src
     } catch(err) {
       return reject(err)
