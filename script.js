7bcf28180b00622ab7ba580c75ced9c450a49a8d
Sergey Staritsyn <47747738+sserge@users.noreply.github.com>
2020-05-12 16:30:23 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
-//(function(){
-(async function(){
+(function(){
+//(async function(){
 
   const canvas = document.getElementById('canvas')
   const context = canvas.getContext('2d')
   
   canvas.width = 1000
-  canvas.height = 750
+  canvas.height = 1000
   
-  const image = await loadImage('./i2.png')
-  console.log('111111')
-//  console.log(image)
+  var img = new Image
+  img.onload = function() {
+    context.drawImage(img)
+      0, 0, img.width, img.height,
+      0, 0, canvas.width, canvas.height
+    )
+  }
+  img.src = "./i2.png"
+  
+  // загрузка через функцию loadImage из additional.js не работает
+  //const image = await loadImage('./i2.png'); // обертка должна быть с ключевым словом async
   
   
 //  update()
@@ -26,16 +34,16 @@
 //  }
   
   
-  
+//// ==========================================================================================
+///   Дополнительные функции   (вынесены в отдельный файл additional.js
+//   ==========================================================================================
 function loadImage (src) {
   return new Promise((resolve, reject) => {
     try {
       const image = new Image
       image.onLoad = () => return resolve(image)
       image.src = src
-      console.log('asdd')
     } catch(err) {
-      console.log('ERROR')
       return reject(err)
     }
   })
